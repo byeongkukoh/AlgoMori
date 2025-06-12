b372aa2b4089d3610280caf29691f470a34a5229
byeongkukoh <byeongkukoh@gmail.com>
2025-06-12 11:46:07 +0900

    fix: Koyeb 코드 제거 - 자체 서버로 변경하며 Koyeb 배포를 위한 코드를 삭제하였습니다.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import discord
-import asyncio
 
-from aiohttp import web
 from discord.ext import commands
 from config import DISCORD_BOT_TOKEN
 from cogs.recommender_cog import RecommenderCog
@@ -18,29 +16,5 @@ async def on_ready():
     # Start the daily recommendation task
     await bot.add_cog(RecommenderCog(bot))
 
-
-# aiohttp 핑 서버
-async def ping(request):
-    return web.Response(text="Pong!")
-
-async def run_web():
-    app = web.Application()
-    app.router.add_get('/ping', ping)
-    runner = web.AppRunner(app)
-
-    await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", 8000)
-
-    await site.start()
-    print("Health check server running on http://0.0.0.0:8000")
-
-
-# bot과 server 동시 실행
-async def main():
-    await asyncio.gather(
-        bot.start(DISCORD_BOT_TOKEN),
-        run_web()
-    )
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot.run(DISCORD_BOT_TOKEN)
